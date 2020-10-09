657e94877591b376e1fd7d6a85d86bc77c9fb458
Rebekah Galliano <rebekahgalliano@gmail.com>
2020-10-09 12:23:45 +0100

    Passing first test - added vars for new_diary and diary.
    .***
    
    Pending: (Failures listed here are expected and do not affect your suite's status)
    
      1) SecretDiary when locked refuses to be written
         # Not yet implemented
         # ./spec/secret_diary_spec.rb:29
    
      2) SecretDiary when unlocked gets read
         # Not yet implemented
         # ./spec/secret_diary_spec.rb:33
    
      3) SecretDiary when unlocked gets written
         # Not yet implemented
         # ./spec/secret_diary_spec.rb:35
    
    Finished in 0.00349 seconds (files took 0.07785 seconds to load)
    4 examples, 0 failures, 3 pending

--- spec/secret_diary_spec.rb
+++ spec/secret_diary_spec.rb
@@ -18,8 +18,13 @@ require_relative '../lib/secret_diary'
 ### WARNING ###
 
 RSpec.describe SecretDiary do
+  let(:new_diary) { SecretDiary.new(diary) }
+  let(:diary) { double :diary }
   context "when locked" do
-    pending "refuses to be read"
+    it "refuses to be read" do
+      expect(new_diary.read).to eq "Go away!"
+    end
+  
 
     pending "refuses to be written"
   end
